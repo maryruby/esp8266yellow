cba44261c9e6b17a896b89dad1c99ff612da42e3
Mary Danilchenko <mary.ruby@yandex.ru>
2020-05-19 01:37:04 +0300

    worked LED's on 595

--- init.lua
+++ init.lua
@@ -1,100 +1,77 @@
-wifi.setmode(wifi.STATION)
-station_cfg={}
-station_cfg.ssid="barmaglot"
-station_cfg.pwd="br0Nenosets"
-station_cfg.save=true
-wifi.sta.config(station_cfg);
-print(wifi.sta.getip());
+latch=8
+spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, spi.DATABITS_8, 0);
+gpio.mode(latch, gpio.OUTPUT)
 
-led1 = 3;
-led2 = 5;
-button1 = 4;
-gpio.mode(led1, gpio.OUTPUT)
-gpio.mode(led2, gpio.OUTPUT)
-restart=0;
-lighton=0
-gpio.mode(4, gpio.INPUT);
-button1state = gpio.read(button1);
-print("level pin 4 =", button1state);
-    if button1state == 1 then
-    button1message = 'Включено!' 
-    else button1message = 'Выключено..'
-    end;
-gpio.write(led1, gpio.HIGH);
-gpio.write(led2, gpio.HIGH);
+gpio.write(latch, gpio.HIGH)
+tmr.delay(30)
+gpio.write(latch, gpio.LOW)
+tmr.delay(30)
+gpio.write(latch, gpio.HIGH)
+tmr.delay(45)
+
+data = {1,2,4};
+
+-- Config
+local MISO = 5            --> GPIO14
+local CLK = 6             --> GPIO12
+local CS = 7              --> GPIO13
+local duration = 3000     --> 3 seconds
+local i = 0
+local result = 0
+
+-- Pin Initialization
+gpio.mode(CS, gpio.OUTPUT)
+gpio.mode(CLK, gpio.OUTPUT)
+gpio.mode(MISO, gpio.INPUT)
+gpio.write(CS,gpio.HIGH)
+
+-- Function to read SPI
+function readSPI()
+    gpio.write(CS, gpio.LOW)      -->Activate the chip
+    tmr.delay(1)                  -->1us Delay
+
+    gpio.write(CLK, gpio.HIGH)    -->First bit is dummy, ignore it(refer MAX6675 datasheet)
+    tmr.delay(2)
+    gpio.write(CLK, gpio.LOW)
+    tmr.delay(2)
+
+    result = 0
+   
+   for i=15,1,-1
+    do
+      result = bit.lshift(result, 1)
+      result = bit.bor(result ,(bit.band(gpio.read(MISO),0x01)))
+      gpio.write(CLK, gpio.HIGH)   
+      tmr.delay(2)
+      gpio.write(CLK, gpio.LOW)
+      tmr.delay(2)
+    end
+    if(bit.isset(result,2)) then
+        print("Sensor not connected")
+        gpio.write(CS, gpio.HIGH)
+        do return end
+    end
+       
+    gpio.write(CS, gpio.HIGH)
+    print(bit.rshift(result,3))
+
+end
+
+-- Create an interval
 
 
 
-print('\nAll About Circuits main.lua\n')
-mytimer=tmr.create()
-mytimer:alarm(1000, tmr.ALARM_AUTO, function()
-   if wifi.sta.getip() == nil then
-      print("Connecting to AP...\n")
-   else
-      ip, nm, gw=wifi.sta.getip()
-      print("IP Info: \nIP Address: ",ip)
-      print("Netmask: ",nm)
-      print("Gateway Addr: ",gw,'\n')
-      mytimer:stop(0)
-   end
-end)
+i=1
+for i=1,3,1 do
+   print(data[i]);
+   sendData(data[i]);
+   tmr.delay(800000);
 
- -- Start a simple http server
-srv=net.createServer(net.TCP, 1000)
-srv:listen(80,function(conn)
-  conn:on("receive",function(client,request)
-    print("request ",request)
-     local buf = "";
-        local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
-        if(method == nil)then
-            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
-        end
-        local _GET = {}
-        if (vars ~= nil)then
-            for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
-                _GET[k] = v
-            end
-        end
-     conn:send(string.format('HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nCache-Control: private, no-store\r\n\r\n\
-   <!DOCTYPE HTML>\
-<html>\
- <head>\
-        <meta charset="UTF-8" />\
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> \
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> \
-        <title>My control</title>\
-   </head><body>\
-        <div class="container">\
-            <section class="color-2">\
-                <p>\
-                    <a href=\"?pin=ON1\"><button class="btn btn-4 btn-4a">Led1 ON</button></a>\
-                    <a href=\"?pin=ON2\"><button class="btn btn-4 btn-4a">Led2 ON</button></a>\
-                </p>\
-                <p>\
-                    <a href=\"?pin=OFF1\"><button class="btn btn-5 btn-5a">Led1 OFF</button></a>\
-                    <a href=\"?pin=OFF2\"><button class="btn btn-5 btn-5a">Led2 OFF</button></a>\
-                </p>\
-                <p> My button is: %s </p>\
-            </section>\
-        </div>\
-</body></html>', button1message))
+   gpio.write(latch, gpio.LOW)
+   spi.recv(1, 1)
+   print(string.byte(read))
+   readSPI();
+   gpio.write(latch, gpio.HIGH)
+   tmr.delay(100)
 
-  local _on,_off = "",""
-        if(_GET.pin == "ON1")then
-              lighton=0;
-              gpio.write(led1, gpio.LOW);
-        elseif(_GET.pin == "OFF1")then
-              lighton=1;
-              gpio.write(led1, gpio.HIGH);
-        elseif(_GET.pin == "ON2")then
-              lighton=0;
-              gpio.write(led2, gpio.LOW);
-        elseif(_GET.pin == "OFF2")then
-              lighton=1;
-              gpio.write(led2, gpio.HIGH);
-        end
-        
-  conn:on("sent",function(conn) conn:close() end)
-collectgarbage();
-end)  
-end)
+end
