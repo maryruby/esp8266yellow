8fbabde578b75ac315b43c3bdc02446b6c6ae737
Mary Danilchenko <mary.ruby@yandex.ru>
2020-05-21 18:39:33 +0300

    В init.lua этой версии заработал наконец 165 регистр, только защелку пришлось повесить на GPIO15 (8)

--- init.lua
+++ init.lua
@@ -1,77 +1,35 @@
-latch=8
-spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, spi.DATABITS_8, 0);
-gpio.mode(latch, gpio.OUTPUT)
+latch165 = 8;
+MISO = 6;          
+CLK = 5;  
+i = 0;
+result = 0;
 
-gpio.write(latch, gpio.HIGH)
-tmr.delay(30)
-gpio.write(latch, gpio.LOW)
-tmr.delay(30)
-gpio.write(latch, gpio.HIGH)
-tmr.delay(45)
+gpio.mode(latch165, gpio.OUTPUT);
+gpio.write(latch165, gpio.LOW);
+gpio.write(latch165, gpio.HIGH);
+tmr.delay(100000);
 
-data = {1,2,4};
+gpio.mode(MISO, gpio.INPUT); 
 
--- Config
-local MISO = 5            --> GPIO14
-local CLK = 6             --> GPIO12
-local CS = 7              --> GPIO13
-local duration = 3000     --> 3 seconds
-local i = 0
-local result = 0
+gpio.mode(CLK, gpio.OUTPUT); 
+gpio.write(CLK, gpio.LOW); 
 
--- Pin Initialization
-gpio.mode(CS, gpio.OUTPUT)
-gpio.mode(CLK, gpio.OUTPUT)
-gpio.mode(MISO, gpio.INPUT)
-gpio.write(CS,gpio.HIGH)
+gpio.write(latch165, gpio.LOW); 
+tmr.delay(300000);
+gpio.write(CLK, gpio.HIGH);  
+tmr.delay(100000);
+gpio.write(CLK, gpio.LOW);
+tmr.delay(100000); 
+gpio.write(latch165, gpio.HIGH); 
 
--- Function to read SPI
-function readSPI()
-    gpio.write(CS, gpio.LOW)      -->Activate the chip
-    tmr.delay(1)                  -->1us Delay
+for i=8,1,-1 do
+    result = gpio.read(MISO); 
+    print(result); 
+    gpio.write(CLK, gpio.HIGH); 
+    tmr.delay(100000);
+    gpio.write(CLK, gpio.LOW);
+    tmr.delay(100000);
+end;
 
-    gpio.write(CLK, gpio.HIGH)    -->First bit is dummy, ignore it(refer MAX6675 datasheet)
-    tmr.delay(2)
-    gpio.write(CLK, gpio.LOW)
-    tmr.delay(2)
-
-    result = 0
-   
-   for i=15,1,-1
-    do
-      result = bit.lshift(result, 1)
-      result = bit.bor(result ,(bit.band(gpio.read(MISO),0x01)))
-      gpio.write(CLK, gpio.HIGH)   
-      tmr.delay(2)
-      gpio.write(CLK, gpio.LOW)
-      tmr.delay(2)
-    end
-    if(bit.isset(result,2)) then
-        print("Sensor not connected")
-        gpio.write(CS, gpio.HIGH)
-        do return end
-    end
-       
-    gpio.write(CS, gpio.HIGH)
-    print(bit.rshift(result,3))
-
-end
-
--- Create an interval
-
-
-
-i=1
-for i=1,3,1 do
-   print(data[i]);
-   sendData(data[i]);
-   tmr.delay(800000);
-
-   gpio.write(latch, gpio.LOW)
-   spi.recv(1, 1)
-   print(string.byte(read))
-   readSPI();
-   gpio.write(latch, gpio.HIGH)
-   tmr.delay(100)
-
-end
+--for i=10,1,-1 do gpio.write(CLK, gpio.HIGH) tmr.delay(100000) gpio.write(CLK, gpio.LOW) tmr.delay(100000) end
+ 
